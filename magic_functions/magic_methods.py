d7206cd2b2a228862ae61f26adc0a896ff988b95
zschuster <ztschuster@gmail.com>
2019-03-23 10:10:06 -0400

    work through unary operators

---
+++ magic_functions/magic_methods.py
@@ -0,0 +1,31 @@
+# The purpose of this script is to get acquainted with unary operator methods
+
+
+class Operations:
+	def __init__(self, *args):
+		if len(args) == 0:
+			self.numbers = (0, 0)
+		else:
+			self.numbers = args
+
+	def __add__(self, other):
+		added = tuple(x + y for x, y in zip(self.numbers, other.numbers))
+		return Operations(*added)
+
+	def __mul__(self, other):
+		mul = (x * y for x, y in zip(self.numbers, other.numbers))
+		return Operations(*mul)
+
+
+# define objects for addition
+obj1 = Operations(3, 4, 5)
+obj2 = Operations(7, 8, 9)
+
+print("Addition")
+print((obj1 + obj2).numbers)
+
+obj3 = Operations(3, 3, 3)
+obj4 = Operations(5, 6, 7)
+
+print("\nMultiplication")
+print((obj3 * obj4).numbers)
