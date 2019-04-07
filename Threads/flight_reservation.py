9a21756d8decfe2e7835593d9e97df97109e64e6
zschuster <ztschuster@gmail.com>
2019-04-07 18:32:04 -0400

    work on multi-threading

--- Threads/flight_reservation.py
+++ Threads/flight_reservation.py
@@ -1,11 +1,17 @@
-from threading import Thread
+from threading import Thread, Lock
 
 
 # create a flight reservation class to purchase tickets
 class FlightReservation:
 
+	# create a lock for a given thread
+	lock = Lock()
+
 	def __init__(self, tickets_left):
 		self.tickets_left = tickets_left
+
+	# acquire / set the thread lock
+	lock.acquire()
 
 	def buy(self, tickets_requested):
 		if self.tickets_left >= tickets_requested:
@@ -13,6 +19,9 @@ class FlightReservation:
 			self.tickets_left -= tickets_requested
 		else:
 			print('You requested too many tickets!')
+
+	# release the lock
+	lock.release()
 
 
 # we could run into problems in this scenario as all threads are calling the
